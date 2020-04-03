62341bd45f5a5f51139aaa4c55d8a97415433651
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-03 14:23:54 +0100

    add: eightball responses

--- magicEightBall.js
+++ magicEightBall.js
@@ -9,3 +9,30 @@ console.log(`You asked: ${userQuestion}`);
 let randomNumber = Math.floor(Math.random() * 8);
 
 let eightBall = '';
+
+switch(randomNumber) {
+  case 1 :
+    eightBall = 'It is certain';
+    break;
+  case 2 :
+    eightBall = 'It is decidly so';
+    break;
+  case 3 :
+    eightBall = 'Reply hazy try again';
+    break;
+  case 4 :
+    eightBall = 'Cannot predict now';
+    break;
+  case 5 :
+    eightBall = 'Do not count on it';
+    break;
+  case 6 :
+    eightBall = 'My sources say no';
+    break;
+  case 7 :
+    eightBall = 'Outlook not so good';
+    break;
+  case 8 :
+    eightBall = 'Signs point to yes';
+    break;
+}
