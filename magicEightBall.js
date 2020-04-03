8e497951c39fed232cf594b7806872c5e0c9f8fe
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-03 14:19:46 +0100

    add: eightBall variable

--- magicEightBall.js
+++ magicEightBall.js
@@ -7,3 +7,5 @@ const userQuestion = 'Will the quarantine end before May 15th?';
 console.log(`You asked: ${userQuestion}`);
 
 let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
