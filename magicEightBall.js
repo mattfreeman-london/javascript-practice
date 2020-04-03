9898a59e78d11e77eabb25842029a56d66c6910b
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-03 14:19:15 +0100

    add: random number generator

--- magicEightBall.js
+++ magicEightBall.js
@@ -5,3 +5,5 @@ userName ? console.log(`Hello, ${userName}`) : console.log('Hello!');
 const userQuestion = 'Will the quarantine end before May 15th?';
 
 console.log(`You asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
