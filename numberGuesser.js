6ae84b559362f7e33960767298d7fbec546a7421
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-03 12:26:34 +0100

    add compareGuess function

--- numberGuesser.js
+++ numberGuesser.js
@@ -5,3 +5,10 @@ let currentRoundNumber = 1;
 function generateTarget() {
   return Math.floor(Math.random) * 9
 };
+
+function compareGuesses(humanGuess, computerGuess, secretTarget) {
+  if Math.abs(humanGuess - secretTarget) <= Math.abs(computerGuess - secretTarget)
+    return true;
+} else {
+    return false;
+};
