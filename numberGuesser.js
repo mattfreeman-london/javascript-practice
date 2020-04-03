c236b35b1bd632ee37c2de093a04d0cebf223675
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-03 12:46:33 +0100

    add: updateScore

--- numberGuesser.js
+++ numberGuesser.js
@@ -2,12 +2,20 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-function generateTarget() {
-  return Math.floor(Math.random) * 9
+let generateTarget = () => {
+  return Math.floor(Math.random() * 9);
 };
 
 let compareGuesses = (human, computer, target) => {
   const userG = Math.abs(target - human);
   const computerG = Math.abs(target - computer);
   return userG <= computerG;
+};
+
+let updateScore = winner => {
+  if (winner === 'human') {
+    humanScore += 1;
+  } else if (winner === 'computer') {
+    computerScore += 1;
+  }
 };
