0452f301445811572deb22e3c10889f46bdd0848
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-03 13:11:43 +0100

    add: early years calculation

--- dogYears.js
+++ dogYears.js
@@ -3,3 +3,9 @@ const myAge = 40;
 
 // set early years variable
 let earlyYears = 2;
+
+// as per instructions, first two years of dog life are 10.5 human years
+earlyYears *= 10.5;
+
+// to account for the first two years
+let laterYears = myAge -2;
