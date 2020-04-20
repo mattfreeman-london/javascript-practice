d39456c24d50f39c0158f2479b1a9f135ac15efe
Matt Freeman <53695672+mattfreeman-london@users.noreply.github.com>
2020-04-20 14:12:10 +0100

    Create mediaLibrary.js
    add file to create a simple Media library, using parent and child classes

---
+++ mediaLibrary.js
@@ -0,0 +1,89 @@
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+  get title() {
+    return this._title
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut
+  }
+  get ratings() {
+    return this._ratings
+  }
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this.isCheckedOut;
+  }
+  getAverageRating() {
+    let ratingsSum = this.ratings.reduce((currentSum, rating) => currentSum + rating, 0);
+    const lengthOfArray = this.ratings.length;
+    return ratingsSum / lengthOfArray;
+  }
+  addRating(newRating) {
+    this.ratings.push(newRating);
+  }
+  set isCheckedOut(newIsCheckedOut) {
+    this._isCheckedOut = newIsCheckedOut;
+  }
+}
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  get author() {
+    return this._author
+  }
+  get pages() {
+    return this._pages
+  }
+}
+
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  get director() {
+    return this._director
+  }
+  get runTime() {
+    return this._runTime
+  }
+}
+
+class CD extends Media {
+  constructor(artist, title, tracks) {
+    super(title);
+    this._artist = artist;
+    this._tracks = tracks;
+  }
+  get artist() {
+    return this._director
+  }
+  get tracks() {
+    return this._tracks
+  }
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating());
